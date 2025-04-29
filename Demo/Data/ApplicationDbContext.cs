716602d9b373b773daeccb5da6f0d7b6dd5d8322
Safin-Sarker <safinsarker1122@gmail.com>
2025-04-29 14:42:17 +0200

    project SetUp Done

---
+++ Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
